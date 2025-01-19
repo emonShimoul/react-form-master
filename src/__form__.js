bf11f5c6c137f5acbf20e60569a4fcf7550e0336
emonShimoul <emonshimoul@gmail.com>
2025-01-20 02:32:50 +0600

    explore Ref and controlled and uncontrolled components

---
+++ src/__form__.js
@@ -0,0 +1,12 @@
+/**
+ * 1. use form tag and onSubmit handler with event(e)
+ * form access: e.target.[name of the input field].value
+ *
+ * 2. controlled element: use individual field state for each input field
+ *
+ * 3. controlled element(2): one object in a state holding all the input fields value
+ *
+ * 4. uncontrolled element: use useRef hook to create a reference to the element and access value. ex: nameRef.current.value
+ *
+ *
+ */
